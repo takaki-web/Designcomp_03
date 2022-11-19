799c535b02f6cada3f8c2ba8fc05351d0d6b111b
takaki-web <104483390+takaki-web@users.noreply.github.com>
2022-11-19 11:23:07 +0900

    js-prettier

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 // ハンバーガーメニュー
 $(function () {
-  $('.js-l-header__burger-btn').click(function () {
-    $(this).toggleClass('active')
+  $(".js-l-header__burger-btn").click(function () {
+    $(this).toggleClass("active");
 
-    if ($(this).hasClass('active')) {
-      $('.js-l-header__burger-menu').addClass('active')
-      $('.js-l-header').addClass('height')
+    if ($(this).hasClass("active")) {
+      $(".js-l-header__burger-menu").addClass("active");
+      $(".js-l-header").addClass("height");
     } else {
-      $('.js-l-header__burger-menu').removeClass('active')
-      $('.js-l-header').removeClass('height')
+      $(".js-l-header__burger-menu").removeClass("active");
+      $(".js-l-header").removeClass("height");
     }
-  })
-})
+  });
+});
 // ページ内リンクに飛ぶ時にリンククリックしたらハンバーガーメニューが閉じるように
-$('.l-header__burger-menu-link').on('click', function (event) {
-  $('.js-l-header__burger-btn').trigger('click')
-})
+$(".l-header__burger-menu-link").on("click", function (event) {
+  $(".js-l-header__burger-btn").trigger("click");
+});
 
 // ハンバーガーメニューfooter-ver
 $(function () {
-  $('.js-l-footer__burger-btn').click(function () {
-    $(this).toggleClass('active')
+  $(".js-l-footer__burger-btn").click(function () {
+    $(this).toggleClass("active");
 
-    if ($(this).hasClass('active')) {
-      $('.js-l-footer__burger-menu').addClass('active')
-      $('.js-l-footer__wrapper').addClass('height')
+    if ($(this).hasClass("active")) {
+      $(".js-l-footer__burger-menu").addClass("active");
+      $(".js-l-footer__wrapper").addClass("height");
     } else {
-      $('.js-l-footer__burger-menu').removeClass('active')
-      $('.js-l-footer__wrapper').removeClass('height')
+      $(".js-l-footer__burger-menu").removeClass("active");
+      $(".js-l-footer__wrapper").removeClass("height");
     }
-  })
-})
+  });
+});
 // ページ内リンクに飛ぶ時にリンククリックしたらハンバーガーメニューが閉じるようにfooter-ver
-$('.l-footer__burger-menu-link').on('click', function (event) {
-  $('.js-l-footer__burger-btn').trigger('click')
-})
+$(".l-footer__burger-menu-link").on("click", function (event) {
+  $(".js-l-footer__burger-btn").trigger("click");
+});
